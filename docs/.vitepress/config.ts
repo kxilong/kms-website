96d9463efd5d0c150fe902bf2a8f2ade143c29a6
lison <740394554@qq.com>
2024-05-07 17:22:56 +0800

    modify deploy.yml

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/kms-website/",
   title: "知识笔记",
   head: [
     [
